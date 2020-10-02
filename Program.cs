40de95ac84521766ea5f15d2ad9d8fca423c26a0
Ryan <zelda0169@gmailcom>
2020-10-02 16:13:49 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTImedrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Current time is {0}", DateTime.Now);
+            Console.WriteLine("Please choose a number");
+            int X = Convert.ToInt32(Console.ReadLine());
+            var Y = DateTime.Now.AddHours(X);
+            Console.WriteLine("in {0} hours it will be {1} " , X ,  Y);
+            Console.ReadLine();
+
+        }
+    }
+}
